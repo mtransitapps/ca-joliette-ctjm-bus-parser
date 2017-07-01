3d1b50950384531fa20dbb807e62f915a0c5a99e
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-01 11:36:58 -0400

    New URL.

--- src/org/mtransit/parser/ca_joliette_ctjm_bus/JolietteCTJMBusAgencyTools.java
+++ src/org/mtransit/parser/ca_joliette_ctjm_bus/JolietteCTJMBusAgencyTools.java
@@ -17,8 +17,8 @@ import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
 
-// https://www.amt.qc.ca/en/about/open-data
-// http://www.amt.qc.ca/xdata/crtl/google_transit.zip
+// https://rtm.quebec/en/about/open-data
+// https://rtm.quebec/xdata/crtl/google_transit.zip
 public class JolietteCTJMBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(String[] args) {
